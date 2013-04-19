baae894d1166ecd884e93135665da516e187130e
gGgGhost <gough.ad@gmail.com>
2013-04-19 23:25:38 +0100

    Added comments to the product display page

--- shop/products/index.php
+++ shop/products/index.php
@@ -1,13 +1,16 @@
 <?php
-
+// Link into the api
 $directory = __DIR__;
 require_once "$directory/../../api/products/index.php";
 
+// Hook up to database
 $db_link = getConnected();
-$id = $_GET['id'];
 
+// Get the desired product, based on GET parameter
+$id = $_GET['id'];
 $product = getProduct($id, $db_link);
 
+// Display product details
 printProductPagePurchasable($product);
 
 // End connection
